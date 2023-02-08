d1d8ceadf8622cb9e3af52225581f93d0c1bc3ee
suhaibkhan699 <khansuhaib699@gmail.com>
2023-02-08 01:49:38 +0000

    folder structure updated

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json())
 
 // for url encoded data
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/images', express.static(path.join('backend/images')))
+app.use('/images', express.static(path.join('images')))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
